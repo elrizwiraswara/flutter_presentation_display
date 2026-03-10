c799117abfc0d5dc25c8033eedf4404ab492e521
Emre Gültekir <gltkr.emre@gmail.com>
2026-03-10 13:19:27 +0300

    Remove SYSTEM_ALERT_WINDOW permission and clean up unused imports in PresentationDisplay

--- android/src/main/kotlin/com/elriztechnology/flutter_presentation_display/FlutterPresentationDisplayPlugin.kt
+++ android/src/main/kotlin/com/elriztechnology/flutter_presentation_display/FlutterPresentationDisplayPlugin.kt
@@ -140,7 +140,6 @@ class FlutterPresentationDisplayPlugin : FlutterPlugin, ActivityAware, MethodCha
         }
     }
 
-
     private fun transferDataToPresentation(call: MethodCall, result: MethodChannel.Result) {
         try {
             flutterEngineChannel?.invokeMethod("transferDataToPresentation", call.arguments)
